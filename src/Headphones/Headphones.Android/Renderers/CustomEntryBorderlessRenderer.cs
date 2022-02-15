086c11dc75e17f253f43babc93cc4c36dfbc2ee7
Jelena Panov <jelena.panov@outlook.com>
2022-02-15 20:28:12 +0100

    Search bar

---
+++ src/Headphones/Headphones.Android/Renderers/CustomEntryBorderlessRenderer.cs
@@ -0,0 +1,26 @@
+﻿using Android.Content;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Headphones.Droid.Renderers;
+using Headphones.Renderers;
+
+[assembly: ExportRenderer(typeof(CustomEntryBorderless), typeof(CustomEntryBorderlessRenderer))]
+namespace Headphones.Droid.Renderers
+{
+    public class CustomEntryBorderlessRenderer : EntryRenderer
+    {
+        public CustomEntryBorderlessRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                Control.SetBackground(null);
+            }
+        }
+    }
+}
--- src/Headphones/Headphones/App.xaml.cs
+++ src/Headphones/Headphones/App.xaml.cs
@@ -10,7 +10,7 @@ namespace Headphones
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
