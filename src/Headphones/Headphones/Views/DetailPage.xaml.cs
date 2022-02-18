375c53e3a8d8ac88bed108581dce101a5d84ca88
Jelena Panov <jelena.panov@outlook.com>
2022-02-19 00:08:20 +0100

    Details page

---
+++ src/Headphones/Headphones/Views/DetailPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Headphones.Models;
+using Headphones.ViewModels;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Headphones.Views
+{
+    public partial class DetailPage : ContentPage
+    {
+        const uint DURATION_NAME = 550;
+        const uint DURATION_IMG = 300;
+
+        public DetailPage(Phone phone)
+        {
+            InitializeComponent();
+            BindingContext = new DetailPageViewModel(Navigation, phone);
+            lbFirstName.TranslationX = -150;
+            lbLastName.TranslationX = -300;
+            imgPhone.TranslationX = 350;
+        }
+
+        protected override async void OnAppearing()
+        {
+            await Task.WhenAll(
+                lbFirstName.TranslateTo(-150, 0, DURATION_NAME, Easing.Linear),
+                lbFirstName.TranslateTo(-75, 0, DURATION_NAME, Easing.Linear),
+                lbFirstName.TranslateTo(0, 0, DURATION_NAME, Easing.Linear),
+                lbLastName.TranslateTo(-300, 0, DURATION_NAME, Easing.Linear),
+                lbLastName.TranslateTo(-150, 0, DURATION_NAME, Easing.Linear),
+                lbLastName.TranslateTo(0, 0, DURATION_NAME, Easing.Linear)
+            );
+
+            await imgPhone.TranslateTo(350, 0, DURATION_IMG, Easing.Linear);
+            await imgPhone.FadeTo(0.5, DURATION_IMG, Easing.Linear);
+            await imgPhone.TranslateTo(175, 0, DURATION_IMG, Easing.Linear);
+            await imgPhone.TranslateTo(0, 0, DURATION_IMG, Easing.Linear);
+            await imgPhone.FadeTo(1, DURATION_IMG, Easing.Linear);
+        }
+    }
+}
